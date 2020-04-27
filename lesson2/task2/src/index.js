14cc035ca86f5cf200e60e91b3199c011c11ec1b
nikolay07 <nikolay0701@ukr.net>
2020-04-27 18:29:26 +0300

    1

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -1,50 +1,36 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const webpack = require("webpack");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === "production";
-    const config = {
-        entry: "./src/index.js",
-        output: {
-            filename: "bundle.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /.js$/,
-                    use: ["babel-loader"]
-                },
-                {
-                    test: /.s?css$/,
-                    use: [
-                        isProduction
-                            ? MiniCssExtractPlugin.loader
-                            : "style-loader",
-                        "css-loader",
-                        "sass-loader",
-                    ]
-                }
-            ],
-        },
-        plugins: [
-            new webpack.ProgressPlugin(),
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: "./src/index.html"
-            }),
-        ],
-        devServer: {
-            hot: true
-        }
-    };
+const rootElem = document.querySelector('#root')
+const style = {color: 'red', fontWeight: 700};
+const element = (
+<main className="page">
+        <form className="login-form">
+            <h1 className="form-title">Profile</h1>
+            <div className="form-control">
+                <label className="form-label" htmlFor="name">Name</label>
+                <input
+                    className="form-input"
+                    type="text"
+                    id="name"
+                    name="name"
+                    value="anonymous"
+                />
+            </div>
+            <div className="form-control">
+                <label className="form-label" id="age" htmlFor="age">Age</label>
+                <input
+                    className="form-input"
+                    type="number"
+                    value="17"
+                    name="age"
+                />
+                <span style={style}>To young</span>
+            </div>
+            <button className="submit-button" type="submit">Submit</button>
+        </form>
+    </main>
+);
 
-    if (isProduction) {
-        config.plugins.push(new MiniCssExtractPlugin({
-            filename: "[name].css",
-        }));
-    }
-
-    return config;
-};
+ReactDOM.render(element, rootElem);
