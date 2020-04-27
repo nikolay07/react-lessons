73d6ed849e6d942fdb9a22a8cb6e568f8ae90334
nikolay07 <nikolay0701@ukr.net>
2020-04-27 16:16:34 +0300

    1

--- lesson1/hw1/src/index.js
+++ lesson1/hw1/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const rootElement = document.querySelector('#root');
+const rootElem = document.querySelector('#root');
 
-const createElement = <div className="title">Todo List</div>;
+const todoListElem = (
+  <h1 className="title">Todo List</h1>
+);
 
-ReactDOM.render(createElement, rootElement);
-
+ReactDOM.render(todoListElem, rootElem);
 
