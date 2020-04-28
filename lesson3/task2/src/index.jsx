752d4b6ce363b8b7de84f7de9f2f20eeab820daf
nikolay07 <nikolay0701@ukr.net>
2020-04-28 10:27:02 +0300

    1

---
+++ lesson3/task2/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+
+ReactDOM.render(
+  <Search name="Nik"/>,
+  rootElement
+)
