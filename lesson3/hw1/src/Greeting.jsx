7639e883deb4218f374d453f15f81bfa16f75853
nikolay07 <nikolay0701@ukr.net>
2020-04-28 17:59:22 +0300

    1

--- lesson3/hw1/src/Greeting.jsx
+++ lesson3/hw1/src/Greeting.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const calcAge = (date) =>
 Math.floor(((new Date().getTime() - new Date(date)) / (24 * 3600 * 365 * 1000)))
 
-function Greeting(props) {
-  return (<div className="reeting">My name is {props.firstName} {props.lastName}. I`m {calcAge(props.birthDate)} years old.</div>);
-}
+const Greeting = (props) => {
+  return (  
+        <div className="greeting">
+            {`My name is ${props.firstName} ${props.lastName}. I'm ${calcAge(props.birthDate)} years old`}
+        </div>
+  );
+};
+
 export default Greeting;
