782e11e40a45f0cb837598d5ac8886da57586907
nikolay07 <nikolay0701@ukr.net>
2020-04-28 17:57:07 +0300

    2

--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from "./Greeting.jsx";
 import "./index.scss";
-
 const rootElem = document.querySelector("#root");
-ReactDOM.render(
-    <Greeting firstName='Nik' lastName='Abramson' birthDate={new Date('1986-01-07T10:09:03.819Z')}/>,
-    rootElem
-);
+const GreetingUser = () => {
+  return (
+  <Greeting 
+      firstName='Nik'
+      lastName='Abramson'
+      birthDate={new Date('1986-01-07T10:09:03.819Z')}
+       />
+  );
+}; 
+
+ReactDOM.render(<GreetingUser />, rootElem);
 
