93404cd0b164f47bdc15d0beffea7728163155eb
nikolay07 <nikolay0701@ukr.net>
2020-04-30 01:04:46 +0300

    1

--- lesson5/hw2/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ColorPicker from "./ColorPicker.jsx";
-
-const App = ()=> {
-  return (
-    <ColorPicker />
-  )
-}
- export default App;
--- lesson5/hw2/src/ColorPicker.jsx
+++ lesson5/hw2/src/ColorPicker.jsx
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { Component } from 'react';
+import "./picker.scss";
 
-const ColorPicker = ()=> {
-  return (1)
+class ColorPicker extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+  setTitle = (colorName) => {
+    this.setState({
+      title: colorName,
+    });
+  }
+  
+  reset = () => {
+    this.setState({
+      title: '',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="picker__title">
+          {this.state.title}
+        </div>
+        <div>
+          <button 
+            className="picker__button picker__button_coral"
+            onMouseOver={() => this.setTitle('Coral')} 
+            onMouseOut={this.reset}
+          />
+          <button 
+            className="picker__button picker__button_aqua"
+            onMouseOver={() => this.setTitle('Aqua')} 
+            onMouseOut={this.reset}
+          />
+          <button 
+          className="picker__button picker__button_bisque"
+          onMouseOver={() => this.setTitle('Bisque')} 
+          onMouseOut={this.reset}
+          />
+        </div>
+      </div>
+    );
+  };
 }
- export default ColorPicker;
+
+export default ColorPicker;
