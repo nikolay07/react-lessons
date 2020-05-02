68a1185f99b7be11a14d878442fafdb24f76ab5d
nikolay07 <nikolay0701@ukr.net>
2020-05-02 19:24:55 +0300

    1

--- lesson6/task2/src/index.jsx
+++ lesson6/task2/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import Mailbox from "./MailBox.jsx";
 import "./index.scss";
 const rootElem = document.querySelector("#root");
 
-ReactDOM.render(<App />, rootElem);
+ReactDOM.render(<Mailbox unreadMessages={["a"]} />, rootElem);
