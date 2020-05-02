ed92fd21a70fd05bc2ad1762f88c608f0d6e8550
nikolay07 <nikolay0701@ukr.net>
2020-05-02 20:55:35 +0300

    1

--- lesson6/task3/src/index.jsx
+++ lesson6/task3/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import Page from "./Page.jsx";
 import "./index.scss";
 const rootElem = document.querySelector("#root");
 
-ReactDOM.render(<App />, rootElem);
+ReactDOM.render(<Page />, rootElem);
