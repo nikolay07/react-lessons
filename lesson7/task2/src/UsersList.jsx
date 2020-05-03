c4e93c0410e31cc3aeff1d39879887c113489d1f
nikolay07 <nikolay0701@ukr.net>
2020-05-03 21:17:13 +0300

    1

--- lesson7/task2/src/UsersList.jsx
+++ lesson7/task2/src/UsersList.jsx
@@ -1,26 +1,45 @@
-import React from "react";
-import users from "./Users.jsx";
+import React, { Component } from "react";
+import User from "./User.jsx";
 
-const UsersList = ({ users }) => {
-  return (
-    <div>
-      <button className="btn">-</button>
-      <ul className="users">
-        <li className="user">
-          <span className="user__name">Bob</span>
-          <span className="user__age">21</span>
-        </li>
-        <li className="user">
-          <span className="user__name">Tom</span>
-          <span className="user__age">17</span>
-        </li>
-        ...
-        <li className="user">
-          <span className="user__name">Sam</span>
-          <span className="user__age">45</span>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class UsersList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    sorting: null,
+  };
+  toggleSorting = () => {
+    const newSorting = this.state.sorting === "asc" ? "desc" : "asc";
+    this.setState({
+      sorting: newSorting,
+    });
+  };
+
+  render() {
+    let usersList;
+    if (this.state.sorting) {
+      usersList = this.props.users
+        .slice()
+        .sort((a, b) =>
+          this.state.sorting === "asc" ? a.age - b.age : b.age - a.age
+        );
+    } else {
+      usersList = this.props.users;
+    }
+
+    return (
+      <div>
+        <button className="btn" onClick={this.toggleSorting}>
+          {this.state.sorting || "-"}
+        </button>
+        <ul className="users">
+          {usersList.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
 export default UsersList;
