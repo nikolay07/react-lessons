2f35572673db58d007752fde40667a7aabfaa3ac
nikolay07 <nikolay0701@ukr.net>
2020-05-04 20:35:24 +0300

    1

--- lesson8/task3/src/Clock.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Clock = (props) => {
-  return props;
-};
-export default Clock;
--- lesson8/task3/src/Dimensions.jsx
+++ lesson8/task3/src/Dimensions.jsx
@@ -1,9 +1,33 @@
-import React from "react";
-import Clock from Clock.jsx;
+import React, { Component } from "react";
 
-const Dimensions = () => {
-  return <Clock />;
-};
+class Dimensions extends Component {
+  state = {
+    width: null,
+    height: null,
+  };
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  }
+  onResize = (e) => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${innerWidth} X ${innerHeight}`;
+  };
+  render() {
+    return (
+      <div className="dimensions">{`${this.state.width} px - ${this.state.height} px`}</div>
+    );
+  }
+}
 export default Dimensions;
-// <!-- 708px - width, 798px - heigh -->
-// <div class="dimensions">708px - 789px</div>
