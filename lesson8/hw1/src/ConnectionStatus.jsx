3a5f00851d457fe055bb72a755c3a60339f57232
nikolay07 <nikolay0701@ukr.net>
2020-05-04 21:16:41 +0300

    1

--- lesson8/hw1/src/ConnectionStatus.jsx
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ConnectionStatus = () => {
-  return <div className="status status_offline">Offline</div>;
-};
+class ConnectionStatus extends Component {
+  state = {
+    status: "online",
+  };
+
+  updateStatus = (e) => {
+    this.setState({
+      status: e.type,
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener("online", this.updateStatus);
+    window.addEventListener("offline", this.updateStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("online", this.updateStatus);
+    window.removeEventListener("offline", this.updateStatus);
+  }
+
+  setOfflineStatus = (status) =>
+    `status ${status === "offline" ? "status_offline" : ""}`;
+
+  render() {
+    const { status } = this.state;
+    return <div className={this.setOfflineStatus(status)}>{status}</div>;
+  }
+}
+
 export default ConnectionStatus;
