1a9a58d1c095e5bb409ce43cf186c96944a38857
nikolay07 <nikolay0701@ukr.net>
2020-05-06 00:08:51 +0300

    1

---
+++ lesson9/hw2/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./App";
+import "./index.css";
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Game />, rootElem);
