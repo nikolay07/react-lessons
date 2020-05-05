ae4b1cf2294a6e021ba9400fbac1e14f474f41bf
nikolay07 <nikolay0701@ukr.net>
2020-05-05 23:36:38 +0300

    1

--- lesson9/task3/src/App.jsx
+++ lesson9/task3/src/App.jsx
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import UserForm from "./UserForm";
 
-class App extends Component {
+class App extends React.Component {
+  createUser = (user) => {
+    console.log(user);
+  };
+
   render() {
-    return <UserForm />;
+    return <UserForm onSubmit={this.createUser} />;
   }
 }
 
