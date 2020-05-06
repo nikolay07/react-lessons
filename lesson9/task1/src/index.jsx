b716564da47907ac155925c35edf829cd060e713
nikolay07 <nikolay0701@ukr.net>
2020-05-06 21:21:26 +0300

    1

--- lesson9/task1/src/index.jsx
+++ lesson9/task1/src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Search from "./Search";
 import "./index.scss";
-
 const root = document.querySelector("#root");
 
 ReactDOM.render(<Search />, root);
