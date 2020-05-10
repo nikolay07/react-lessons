b9e5397e2d98c41d7bad6a51338f9d71c756ba06
nikolay07 <nikolay0701@ukr.net>
2020-05-11 00:45:06 +0300

    1

--- lesson10/hw2/src/App.jsx
+++ lesson10/hw2/src/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import UserForm from "./UserForm";
 
 class App extends React.Component {
   render() {
-    return <User />;
+    return <div className=""></div>;
   }
 }
 
