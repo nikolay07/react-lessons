a273e6bebefad423b83b0af174d6e15805b87fda
nikolay07 <nikolay0701@ukr.net>
2020-05-11 20:17:36 +0300

    1

---
+++ lesson12/task1/src/index.js
@@ -0,0 +1,21 @@
+import { initTodoListHandlers } from "./scripts/todoList.js";
+import { renderTasks } from "./scripts/renderer.js";
+import { getTasksList } from "./scripts/tasksGateway.js";
+import { setItem } from "./scripts/storage.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTasksList().then((tasksList) => {
+    setItem("tasksList", tasksList);
+    renderTasks();
+  });
+
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === "tasksList") {
+    renderTasks();
+  }
+};
+
+window.addEventListener("storage", onStorageChange);
