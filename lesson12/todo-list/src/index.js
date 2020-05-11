2ca9ef8d0986b7ad99b469abc23ee08ba1b7172e
nikolay07 <nikolay0701@ukr.net>
2020-05-11 21:23:50 +0300

    1

---
+++ lesson12/todo-list/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+
+ReactDOM.render(
+  <h1 className="title">Todo List</h1>,
+  document.getElementById("root")
+);
