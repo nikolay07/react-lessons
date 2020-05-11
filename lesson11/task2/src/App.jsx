8d661eaebbe2f25a3ba003c44da777efe98d1d6f
nikolay07 <nikolay0701@ukr.net>
2020-05-11 15:01:34 +0300

    1

---
+++ lesson11/task2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Dialog from "./Dialog";
+
+class App extends React.Component {
+  render() {
+    return <Numbers />;
+  }
+}
+
+export default App;
---
+++ lesson11/task2/src/Dialog.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Dialog extends React.Component {
+  render() {
+    return <div className=""></div>;
+  }
+}
+
+export default Dialog;
