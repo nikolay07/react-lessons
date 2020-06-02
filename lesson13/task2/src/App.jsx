6eebf67af86e018b4393bbbd53dfd1c8cc6f3303
nikolay07 <nikolay0701@ukr.net>
2020-06-02 15:33:42 +0300

    1

--- lesson13/task2/src/App.jsx
+++ lesson13/task2/src/App.jsx
@@ -1,30 +1,29 @@
-import React, { Component } from "react";
-import Home from "./Home";
-import Products from "./Products";
+import React from "react";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Products from "./Products";
+import Home from "./Home";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="page">
-        <BrowserRouter>
-          <ul className="navigation">
-            <li className="navigation__item">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="navigation__item">
-              <Link to="/products">Products</Link>{" "}
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/products" component={Products} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products" component={Products} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
 export default App;
--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Product = ({ match }) => {
-  return (
-    <div className="page__content">
-      Product is a {match.params.productId}
-    </div>
-  );
+  return <div className="product">{match.params.productId}</div>;
 };
 
 export default Product;
