b8448df76d6b73f2b86c257cdb206c4108005e19
nikolay07 <nikolay0701@ukr.net>
2020-06-03 19:11:01 +0300

    1

---
+++ lesson13/hw1/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+//import { BrowserRouter, Route, Link } from "react-router-dom";
+//import Users from "./Users";
+//import Home from "./Home";
+
+class App extends Component {
+  render() {
+    return <div>Hello React!</div>;
+  }
+}
+export default App;
